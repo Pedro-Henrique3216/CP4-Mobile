76d0952b2b5f1a811614a44eda5a17e3d65d8bee
Pedro Henrique <ph6704938@gmail.com>
2025-08-11 22:43:47 -0300

    fix: update Layout component to use correct screen name for Home

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,8 @@ import { Stack } from "expo-router";
 
 export default function Layout(){
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Home" options={{ headerShown: false }} />
         </Stack>
     );  
 }
